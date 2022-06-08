6b611ef2fcc0fd3deb9409c0444416379c306023
ricardo <ricardo@rayvenues.com>
2022-06-08 10:20:21 -0400

    Added CKSource\CKFinder

--- src/CKFinderServiceProvider.php
+++ src/CKFinderServiceProvider.php
@@ -3,9 +3,12 @@
 namespace CKSource\CKFinderBridge;
 
 use CKSource\CKFinderBridge\Command\CKFinderDownloadCommand;
+use CKSource\CKFinderBridge\Polyfill\CommandResolver;
 use Illuminate\Support\ServiceProvider;
+use RuntimeException;
 use Symfony\Component\HttpKernel\HttpKernel;
 use Symfony\Component\HttpKernel\Kernel;
+use CKSource\CKFinder\CKFinder;
 
 class CKFinderServiceProvider extends ServiceProvider
 {
@@ -39,8 +42,8 @@ class CKFinderServiceProvider extends ServiceProvider
         }
 
         $this->app->bind('ckfinder.connector', function() {
-            if (!class_exists('\CKSource\CKFinder\CKFinder')) {
-                throw new \Exception(
+            if (!class_exists(CKFinder::class)) {
+                throw new RuntimeException(
                     "Couldn't find CKFinder conector code. ".
                     "Please run `artisan ckfinder:download` command first."
                 );
@@ -49,13 +52,13 @@ class CKFinderServiceProvider extends ServiceProvider
             $ckfinderConfig = config('ckfinder');
 
             if (is_null($ckfinderConfig)) {
-                throw new \Exception(
+                throw new RuntimeException(
                     "Couldn't load CKFinder configuration file. ".
                     "Please run `artisan vendor:publish --tag=ckfinder` command first."
                 );
             }
 
-            $ckfinder = new \CKSource\CKFinder\CKFinder($ckfinderConfig);
+            $ckfinder = new CKFinder($ckfinderConfig);
 
             if (Kernel::MAJOR_VERSION === 4) {
                 $this->setupForV4Kernel($ckfinder);
@@ -68,19 +71,19 @@ class CKFinderServiceProvider extends ServiceProvider
     /**
      * Prepares CKFinder DI container to use version version 4+ of HttpKernel.
      *
-     * @param \CKSource\CKFinder\CKFinder $ckfinder
+     * @param  CKFinder  $ckfinder
      */
     protected function setupForV4Kernel($ckfinder)
     {
-        $ckfinder['resolver'] = function () use ($ckfinder) {
-            $commandResolver = new \CKSource\CKFinderBridge\Polyfill\CommandResolver($ckfinder);
-            $commandResolver->setCommandsNamespace(\CKSource\CKFinder\CKFinder::COMMANDS_NAMESPACE);
-            $commandResolver->setPluginsNamespace(\CKSource\CKFinder\CKFinder::PLUGINS_NAMESPACE);
+        $ckfinder['resolver'] = static function () use ($ckfinder) {
+            $commandResolver = new CommandResolver($ckfinder);
+            $commandResolver->setCommandsNamespace(CKFinder::COMMANDS_NAMESPACE);
+            $commandResolver->setPluginsNamespace(CKFinder::PLUGINS_NAMESPACE);
 
             return $commandResolver;
         };
 
-        $ckfinder['kernel'] = function () use ($ckfinder) {
+        $ckfinder['kernel'] = static function () use ($ckfinder) {
             return new HttpKernel(
                 $ckfinder['dispatcher'],
                 $ckfinder['resolver'],
